52808230f88b924caaa962d641a18f7b1f957fad
vue <18310069523@163.com>
2017-11-02 19:09:25 +0800

    后台管理1

---
+++ wages/demo/src/views/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: '首页'
+    }
+  },
+  created () {
+    this.$http.get('/api/getlist', {}).then((data) => {
+      console.log(data)
+    })
+  }
+}
+</script>
